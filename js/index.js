37d81ab765a89f75396484bfddbf4c0955306123
אביהו פינקו <avihupinko@gmail.com>
2018-08-26 11:27:46 +0300

    added ajax time table

--- js/index.js
+++ js/index.js
@@ -3,10 +3,27 @@ function loadTimeTasks() {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var resp = JSON.parse(this.responseText);
+            fillTimeTaskTable(resp);
         }
     };
+    
+    
     xhttp.open("GET", "https://taskmanagerapi-avihupinko.azurewebsites.net/api/timetask", true);
     xhttp.send();
+}
+
+
+function fillTimeTaskTable(arr) {
+    var htmlString = "";
+    var div = document.getElementById("TimeTaskBody");
+    for (var i = 0; i < arr.length; i++) {
+        var obj = arr[i];
+        htmlString += '<tr class="row100">' +
+            '<td class="column100 column1" data-column="column1">'+obj.Title+'</td>' +
+            '<td class="column100 column2" data-column="column2">'+obj.Description+'</td>' +
+            '<td class="column100 column3" data-column="column3">'+obj.Severity+'</td></tr>';
+    }
+    div.innerHTML = htmlString;
 }
 
 function onload() {
