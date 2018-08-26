aa85ec7b8621b71f4e0970a442409e3bf2794a82
אביהו פינקו <avihupinko@gmail.com>
2018-08-26 12:01:59 +0300

    add ajax call

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 function loadTimeTasks() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -18,7 +36,7 @@ function fillTimeTaskTable(arr) {
     var div = document.getElementById("TimeTaskBody");
     for (var i = 0; i < arr.length; i++) {
         var obj = arr[i];
-        htmlString += '<tr class="row100">' +
+        htmlString += '<tr class="row100"onclick=showTimeTask(' + obj + ')>' +
             '<td class="column100 column1" data-column="column1">'+obj.title+'</td>' +
             '<td class="column100 column2" data-column="column2">' + obj.description + '</td>' +
             '<td class="column100 column3" data-column="column3">' + obj.startDate + '</td>' +
@@ -46,7 +64,7 @@ function fillSeverityTaskTable(arr) {
     var div = document.getElementById("SeverityTaskBody");
     for (var i = 0; i < arr.length; i++) {
         var obj = arr[i];
-        htmlString += '<tr class="row100">' +
+        htmlString += '<tr class="row100" onclick=showSeverityTask('+obj+')>' +
             '<td class="column100 column1" data-column="column1">' + obj.title + '</td>' +
             '<td class="column100 column2" data-column="column2">' + obj.description + '</td>' +
             '<td class="column100 column3" data-column="column3">' + obj.severity + '</td></tr>';
@@ -59,3 +77,14 @@ function onload() {
     loadTimeTasks();
     loadSeverityTasks();
 }
+
+function showSeverityTask(obj) {
+
+    modal.style.display = "block";
+}
+
+function showTimeTask(obj) {
+
+    modal.style.display = "block";
+}
+
